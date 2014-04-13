0ff6155b683c82d038274101a48773f028e05388
motemen <motemen@gmail.com>
2014-04-13 21:27:50 +0900

    browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
 var gulp       = require('gulp'),
     changed    = require('gulp-changed'),
-    typescript = require('gulp-tsc');
+    typescript = require('gulp-tsc'),
+    browserify = require('browserify'),
+    source     = require('vinyl-source-stream'),
+    es         = require('event-stream');
 
-gulp.task('default', ['typescript', 'watch']);
+gulp.task('default', ['browserify', 'watch']);
 
 gulp.task('typescript', function () {
-    gulp.src('src/**/*.ts')
-        .pipe(changed('app/js'))
+    return gulp.src('src/**/*.ts')
+        .pipe(changed('src/js'))
         .pipe(typescript({ emitError: false }))
-        .pipe(gulp.dest('app/js'))
+        .pipe(gulp.dest('src/js'))
+});
+
+gulp.task('browserify', ['typescript'], function () {
+    return es.merge.apply(es,
+        [ 'bg/background.js', 'inject/inject.js', 'options/options.js' ].map(
+            function (path) {
+                return browserify('./src/js/' + path)
+                    .bundle()
+                    .pipe(source(path))
+                    .pipe(gulp.dest('app/js'));
+            }
+        )
+    );
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/**/*.ts', [ 'typescript' ])
+    gulp.watch('src/**/*.ts', [ 'browserify' ])
 });
