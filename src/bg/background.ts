0d55a80013cf3730adaff18d44deebdca50a790c
motemen <motemen@gmail.com>
2014-04-13 23:48:52 +0900

    inject only when status changed to complete

--- src/bg/background.ts
+++ src/bg/background.ts
@@ -2,17 +2,15 @@
 
 import sk = require('../lib/storage-key');
 
+var tabWorking: { [index: number]: boolean } = {};
+
 function getConfigFromUrl (url: string) {
     var storedRules = new sk.StorageKey('options.rules');
     var rules = storedRules.get() || {};
     var hostname = /^\w+:\/\/([^:\/]+)/.exec(url)[1];
 
-    console.log(rules);
-
     for (var p in rules) {
         var regexp = new RegExp('^' + p.replace(/\*/g, '.+?') + '$');
-        console.log(regexp);
-
         if (regexp.test(hostname)) {
             return rules[p];
         }
@@ -21,24 +19,12 @@ function getConfigFromUrl (url: string) {
 
 chrome.tabs.onUpdated.addListener(
     function (tabId, changeInfo, tab) {
-        console.log('onUpdated', tabId);
-
-        if (getConfigFromUrl(tab.url)) {
-            chrome.tabs.executeScript(tabId, { file: 'js/inject/inject.js' })
-        }
-
-        var storedRules = new sk.StorageKey('options.rules');
-        var rules = storedRules.get() || {};
-        var hostname = /^\w+:\/\/([^:\/]+)/.exec(tab.url)[1];
-
-        console.log(rules);
-
-        for (var p in rules) {
-            var regexp = new RegExp('^' + p.replace(/\*/g, '.+?') + '$');
-            console.log(regexp);
-
-            if (regexp.test(hostname)) {
-                chrome.tabs.executeScript(tabId, { code: 'window._j = ' + JSON.stringify(rules[p]) });
+        if (changeInfo.status === 'complete') {
+            if (getConfigFromUrl(tab.url)) {
+                tabWorking[tabId] = true;
+                chrome.tabs.executeScript(tabId, { file: 'js/inject/inject.js' })
+            } else {
+                delete tabWorking[tabId];
             }
         }
     }
@@ -46,9 +32,49 @@ chrome.tabs.onUpdated.addListener(
 
 chrome.runtime.onMessage.addListener(
     function (message: any, sender: chrome.runtime.MessageSender, sendResponse: (any) => void) {
-        var config = getConfigFromUrl(sender.tab.url);
-        if (config) {
-            sendResponse(config);
+        if (tabWorking[sender.tab.id]) {
+            delete tabWorking[sender.tab.id];
+            loadImage(message.url, function (image) {
+                var config = getConfigFromUrl(sender.tab.url);
+                var faviconDataURL = generateFlavouredFaviconDataURL(config, image);
+                sendResponse({ url: faviconDataURL });
+            });
+            return true;
         }
     }
 );
+
+function loadImage(url: string, callback: (HTMLImageElement) => any) {
+    var image = new Image();
+    image.onload = function (e: Event) {
+        callback(image);
+    };
+    image.src = url;
+}
+
+function generateFlavouredFaviconDataURL (config: any, image: HTMLImageElement): string {
+    var w = image.naturalWidth, h = image.naturalHeight;
+
+    function mkCanvasContext () {
+        var canvas = document.createElement('canvas');
+            canvas.width  = w;
+            canvas.height = h;
+        return canvas.getContext('2d');
+    }
+
+    var mask = mkCanvasContext();
+        mask.drawImage(image, 0, 0);
+        mask.globalCompositeOperation = 'source-atop';
+        mask.fillStyle = config.color;
+        mask.fillRect(0, 0, w, h);
+
+    var i = new Image();
+        i.src = mask.canvas.toDataURL();
+
+    var context = mkCanvasContext();
+        context.drawImage(image, 0, 0);
+        context.globalCompositeOperation = 'lighten';
+        context.drawImage(i, 0, 0);
+
+    return context.canvas.toDataURL();
+}
--- src/inject/inject.ts
+++ src/inject/inject.ts
@@ -1,79 +1,23 @@
 /// <reference path="../../DefinitelyTyped/chrome/chrome.d.ts" />
 
-console.log('** START **', document.readyState);
+var faviconLinks = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
+var faviconURL = faviconLinks.length === 0
+               ? '/favicon.ico'
+               : (<HTMLElement>faviconLinks[0]).getAttribute('href');
 
-chrome.runtime.sendMessage({}, function (response: any) {
+var absolutizer = document.createElement('a');
+    absolutizer.href = faviconURL;
 
-    var faviconLinks = document.querySelectorAll('link[rel="icon"], link[rel="shortcut icon"]');
-    var faviconURL = faviconLinks.length === 0
-                   ? '//' + location.host + '/favicon.ico'
-                   : (<HTMLElement>faviconLinks[0]).getAttribute('href');
-
-    console.log(faviconURL);
-
-    // TODO
-    if (faviconURL.indexOf('data:') === -1) {
-        loadImage(faviconURL, function (image) {
-            var faviconDataURL = generateFlavouredFaviconDataURL(image);
-            replaceFavicon(faviconDataURL);
-        });
-    }
-
-    function loadImage(url: string, callback: (HTMLImageElement) => any) {
-        var image = new Image();
-        image.onload = function (e: Event) {
-            console.log('onload', e);
-            callback(image);
-        };
-        image.src = url;
-    }
-
-    function generateFlavouredFaviconDataURL (image: HTMLImageElement): string {
-        var w = image.naturalWidth, h = image.naturalHeight;
-
-        function mkCanvas () {
-            var canvas = document.createElement('canvas');
-            canvas.width  = w;
-            canvas.height = h;
-            return canvas;
-        }
-
-        var canvas = mkCanvas(),
-            mask   = mkCanvas();
-
-        var ctxMask = mask.getContext('2d');
-        ctxMask.drawImage(image, 0, 0);
-        ctxMask.globalCompositeOperation = 'source-atop';
-        ctxMask.fillStyle = response.overlay;
-        ctxMask.fillRect(0, 0, w, h);
-
-        var context = canvas.getContext('2d');
-        context.drawImage(image, 0, 0);
-        // context.globalCompositeOperation = 'source-atop';
-        // context.globalCompositeOperation = 'lighten';
-        context.globalCompositeOperation = 'lighten';
-        /*
-        // context.globalAlpha = 0.5;
-        context.fillStyle = response.overlay;
-        context.fillRect(0, 0, w, h);
-        */
-        var i = new Image();
-        i.src = ctxMask.canvas.toDataURL();
-        context.drawImage(i, 0, 0);
-
-        return canvas.toDataURL();
-    }
-
-    function replaceFavicon (faviconURL: string) {
-        for (var i = 0, el; el = faviconLinks[i]; i++) {
+chrome.runtime.sendMessage({ type: 'favicon-data', url: absolutizer.href }, function (response: any) {
+    for (var i = 0, el; el = faviconLinks[i]; i++) {
+        if (el.parentNode) {
             el.parentNode.removeChild(el);
         }
-
-        var link = document.createElement('link');
-        link.setAttribute('rel', 'icon');
-        link.setAttribute('href', faviconURL);
-
-        document.querySelector('head').appendChild(link);
     }
 
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'icon');
+    link.setAttribute('href', response.url);
+
+    document.querySelector('head').appendChild(link);
 });
